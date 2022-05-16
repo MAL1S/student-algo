eb5df754923ac007ec6e934581f8376948854763
MALIS <karbushev_ivan@mail.ru>
2022-05-16 13:37:59 +0800

    Add project places managing

--- src/main/kotlin/const.kt
+++ src/main/kotlin/const.kt
@@ -12,7 +12,6 @@ val PROJECT_GROUPS_COUNT = (1..3) //количество групп студен
 const val STUDENT_COUNT = 300 //количесто студентов
 const val SUPERVISOR_COUNT = 15 //количество преподавателей
 const val PROJECTS_COUNT = 30 //количество проектов
-//const val PROJECT_STUDENT_CAPACITY = 15 //вместительность проектов
 const val PROJECT_STUDENT_CAPACITY_LOWER_BOUNDARY = 11 //граница "от" для вместимости проектов
 const val PROJECT_STUDENT_CAPACITY_UPPER_BOUNDARY = 12 //граница "до" для вместимости проектов
 const val PROJECT_MIN_CAPACITY = 8 //минимальная вместимость проекта
